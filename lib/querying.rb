1e78f85554f0d57078f3fb8fd078f8e5275b4033
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-21 19:47:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title, characters.species FROM series
   JOIN authors ON series.author_id = authors.id
-  JOIN characters ON characters.author_id = authors.id"
+  JOIN characters ON characters.author_id = authors.id 
+  GROUP BY series.title "
 end
 
 def select_character_names_and_number_of_books_they_are_in
